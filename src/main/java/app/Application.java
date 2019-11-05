4da9ee4f1ff433c00caeef6156a9b57b69af5e09
Bers <berserkingyadis@gmail.com>
2019-11-05 09:36:46 +0100

    smaller cave

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,9 +6,9 @@ import java.awt.event.MouseEvent;
 public class Application {
     public static void main(String[] args) {
 
-        int width = 400;
-        int height = 300;
-        int scale = 3;
+        int width = 100;
+        int height = 100;
+        int scale = 5;
         int step_thresh=5;
 
         int seed = 123;
